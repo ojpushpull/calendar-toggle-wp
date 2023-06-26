00caa0b9b302de4689fdc1d7a067b3ba1cd64734
okera johnson <okerajohnson@gmail.com>
2023-06-26 12:52:08 -0400

    first commit

---
+++ <div class="utmost-devo-title col col-sm.js
@@ -0,0 +1,268 @@
+<div class="utmost-devo-title col col-sm-12 col-xs-12">
+				<div class="entry-date">
+					<!-- <nav class="nav-single"> -->
+
+<a href="#" class="calendar-toggle">June 26 <span class="fa fa-caret-down calendar-icon"></span>
+
+</a>
+
+<!-- </nav> <!-- .nav-single -->					<div id="home-calendar" style="display: none;">
+						
+<table id="wp-calendar" summary="Calendar" style="height: 289px;">
+	<caption>June 26 <i class="fa fa-caret-up"></i></caption>
+	<thead>
+		<tr>
+						<th scope="col" title="Sunday">S</th>
+						<th scope="col" title="Monday">M</th>
+						<th scope="col" title="Tuesday">T</th>
+						<th scope="col" title="Wednesday">W</th>
+						<th scope="col" title="Thursday">T</th>
+						<th scope="col" title="Friday">F</th>
+						<th scope="col" title="Saturday">S</th>
+					</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td colspan="1" class="inactive-month">28</td>
+			<td colspan="1" class="inactive-month">29</td>
+			<td colspan="1" class="inactive-month">30</td>
+			<td colspan="1" class="inactive-month">31</td>
+			<td><a href="/2023/06/01?calendar-redirect=true&amp;post-type=post">1</a></td>
+			<td><a href="/2023/06/02?calendar-redirect=true&amp;post-type=post">2</a></td>
+			<td><a href="/2023/06/03?calendar-redirect=true&amp;post-type=post">3</a></td>
+		</tr>
+		<tr>
+			<td><a href="/2023/06/04?calendar-redirect=true&amp;post-type=post">4</a></td>
+			<td><a href="/2023/06/05?calendar-redirect=true&amp;post-type=post">5</a></td>
+			<td><a href="/2023/06/06?calendar-redirect=true&amp;post-type=post">6</a></td>
+			<td><a href="/2023/06/07?calendar-redirect=true&amp;post-type=post">7</a></td>
+			<td><a href="/2023/06/08?calendar-redirect=true&amp;post-type=post">8</a></td>
+			<td><a href="/2023/06/09?calendar-redirect=true&amp;post-type=post">9</a></td>
+			<td><a href="/2023/06/10?calendar-redirect=true&amp;post-type=post">10</a></td>
+		</tr>
+		<tr>
+			<td><a href="/2023/06/11?calendar-redirect=true&amp;post-type=post">11</a></td>
+			<td><a href="/2023/06/12?calendar-redirect=true&amp;post-type=post">12</a></td>
+			<td><a href="/2023/06/13?calendar-redirect=true&amp;post-type=post">13</a></td>
+			<td><a href="/2023/06/14?calendar-redirect=true&amp;post-type=post">14</a></td>
+			<td><a href="/2023/06/15?calendar-redirect=true&amp;post-type=post">15</a></td>
+			<td><a href="/2023/06/16?calendar-redirect=true&amp;post-type=post">16</a></td>
+			<td><a href="/2023/06/17?calendar-redirect=true&amp;post-type=post">17</a></td>
+		</tr>
+		<tr>
+			<td><a href="/2023/06/18?calendar-redirect=true&amp;post-type=post">18</a></td>
+			<td><a href="/2023/06/19?calendar-redirect=true&amp;post-type=post">19</a></td>
+			<td><a href="/2023/06/20?calendar-redirect=true&amp;post-type=post">20</a></td>
+			<td><a href="/2023/06/21?calendar-redirect=true&amp;post-type=post">21</a></td>
+			<td><a href="/2023/06/22?calendar-redirect=true&amp;post-type=post">22</a></td>
+			<td><a href="/2023/06/23?calendar-redirect=true&amp;post-type=post">23</a></td>
+			<td><a href="/2023/06/24?calendar-redirect=true&amp;post-type=post">24</a></td>
+		</tr>
+		<tr>
+			<td><a href="/2023/06/25?calendar-redirect=true&amp;post-type=post">25</a></td>
+			<td class="active"><a href="/2023/06/26?calendar-redirect=true&amp;post-type=post">26</a></td>
+			<td><a href="/2023/06/27?calendar-redirect=true&amp;post-type=post">27</a></td>
+			<td><a href="/2023/06/28?calendar-redirect=true&amp;post-type=post">28</a></td>
+			<td><a href="/2023/06/29?calendar-redirect=true&amp;post-type=post">29</a></td>
+			<td><a href="/2023/06/30?calendar-redirect=true&amp;post-type=post">30</a></td>
+			<td colspan="1" class="inactive-month">1</td>
+		</tr>
+	</tbody>
+	<tfoot>
+		<tr>
+			<td colspan="3" id="prev">
+				<a href="/2004/05">
+					<i class="fa fa-angle-left cal-angle"></i>
+				</a>
+			</td>
+			<td colspan="3" id="next">
+				<a href="/2004/07">
+					<i class="fa fa-angle-right cal-angle"></i>
+				</a>
+			</td>
+		</tr>
+	</tfoot>
+</table>
+						</div>
+				</div>
+				<h2 class="entry-title">						<span class="bookmark-wrapper login-prompt" data-toggle="modal" data-target="#login-modal">
+		<a href="#" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Login to bookmark"><span class="odb-bookmark"></span></a>
+		<div class="site-login-prompt">
+					</div>
+		</span>
+											<span>Drawing on the Grace of God— Now</span></h2>
+				<h4>By <a href="https://utmost.org/oswald-chambers-bio">Oswald Chambers</a></h4>
+			</div>
+
+			<script type="text/javascript">
+				jQuery(document).ready(function($) {
+					var pubDate = new Date(2004, 6 - 1, 26);
+					var minDate = new Date(2004, 1 - 1, 1);
+					var maxDate = new Date(2004, 12 - 1, 31);
+			
+					var locale = {"weekday":["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"],"weekday_initial":{"Sunday":"S","Monday":"M","Tuesday":"T","Wednesday":"W","Thursday":"T","Friday":"F","Saturday":"S"},"weekday_abbrev":{"Sunday":"Sun","Monday":"Mon","Tuesday":"Tue","Wednesday":"Wed","Thursday":"Thu","Friday":"Fri","Saturday":"Sat"},"month":{"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June","07":"July","08":"August","09":"September","10":"October","11":"November","12":"December"},"month_genitive":{"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June","07":"July","08":"August","09":"September","10":"October","11":"November","12":"December"},"month_abbrev":{"January":"Jan","February":"Feb","March":"Mar","April":"Apr","May":"May","June":"Jun","July":"Jul","August":"Aug","September":"Sep","October":"Oct","November":"Nov","December":"Dec"},"meridiem":{"am":"am","pm":"pm","AM":"AM","PM":"PM"},"text_direction":"ltr","number_format":{"thousands_sep":",","decimal_point":"."}};
+			
+					var month = [];
+			
+					for (var i = 0; i < 12; i++)
+						month[i] = locale.month[(i < 9 ? "0" : "") + (i + 1)];
+			
+					locale.month = month;
+			
+					var dayFlags = 127;
+			
+					var today;
+			
+					var buildCalendar = function(year, month, date) {
+						if (typeof(date) == 'undefined') {
+							date = 26			}
+										today = new Date(2023, month, date );
+									todaysDate = new Date(year, month, date);
+			
+						var numdays = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+						var headoffset = new Date(today.getFullYear(), today.getMonth(), 1).getDay();
+						var tailoffset = (7 - ((headoffset + numdays) % 7)) % 7;
+			
+						var lastMonth = new Date(today.getFullYear(), today.getMonth() , 0).getDate();
+			
+						$('#wp-calendar caption').html(locale.month[today.getMonth()] + " " + today.getDate() + " " + "<i class='fa fa-caret-up'></i>");
+						console.log(today);
+			
+						var tbody = $('#wp-calendar tbody');
+						var trow = $('<tr>');
+			
+						tbody.empty();
+						tbody.append(trow);
+			
+						if (headoffset > 0) {
+							var currentDay = lastMonth - headoffset + 1;
+			
+							while ( currentDay <= lastMonth) {
+								trow.append($('<td>', {
+									'colspan': 1,
+									'class': 'inactive-month'
+								}).html(currentDay));
+			
+								currentDay = currentDay + 1;
+							}
+			
+						}
+			
+						var rowdays = headoffset;
+			
+						var strMonth = (month < 9 ? "0" : "") + (month + 1);
+						var strYear  = (year < 100 ? "20" : "") + year;
+			
+						for (var day = 1; day <= numdays; day++) {
+							var strDay = (day < 10 ? "0" : "") + day;
+			
+							var curDate = new Date(year, month, day);
+							if (rowdays == 7) {
+								trow = $('<tr>');
+								tbody.append(trow);
+								rowdays = 0;
+							}
+			
+							var enableDay = ((1 << rowdays) & dayFlags) > 0;
+			
+							var tcell = $('<td>');
+			
+							if (pubDate.getFullYear() == year && pubDate.getMonth() == month && pubDate.getDate() == day)
+								tcell.addClass('active');
+			
+							trow.append(tcell);
+			
+							if (enableDay && curDate.getTime() <= maxDate.getTime() && curDate.getTime() >= minDate.getTime()) {
+								var path = ['', strYear, strMonth, strDay].join('/');
+			
+								var link = $('<a>', {
+									'href': path + '?calendar-redirect=true&post-type=post'
+								}).text(day);
+			
+								tcell.append(link);
+							} else {
+								tcell.addClass('disabled').text(day);
+							}
+			
+							rowdays++;
+						}
+			
+						if (tailoffset > 0) {
+							var tailDate = 1;
+							while (tailDate <= tailoffset) {
+								trow.append($('<td>', {
+									'colspan': 1,
+									'class': 'inactive-month'
+								}).html(tailDate));
+			
+								tailDate = tailDate + 1;
+							}
+						}
+			
+						if (minDate.getFullYear() < year || minDate.getFullYear() == year && minDate.getMonth() < month)
+							$('#wp-calendar #prev a').show();
+						else
+							$('#wp-calendar #prev a').hide();
+			
+						if (maxDate.getFullYear() > year || maxDate.getFullYear() == year && maxDate.getMonth() > month)
+							$('#wp-calendar #next a').show();
+						else
+							$('#wp-calendar #next a').hide();
+			
+					};
+			
+					buildCalendar(2004, 6 - 1);
+			
+					$('#wp-calendar #prev a').click(function(event) {
+									var year = 2004;
+									var month = today.getMonth();
+			
+						if (0 == month) {
+							month = 11;
+							year--;
+						} else {
+							month--;
+						}
+			
+						buildCalendar(year, month, 1);
+			
+						//$(event).preventDefault();
+						return false;
+					});
+			
+					$('#wp-calendar #next a').click(function(event) {
+									var year = 2004;
+									var month = today.getMonth();
+			
+						if (11 == month) {
+							month = 0;
+							year++;
+						} else {
+							month++;
+						}
+			
+						buildCalendar(year, month, 1);
+			
+						//$(event).preventDefault();
+						return false;
+					});
+					$('a.calendar-toggle').on('click', function (e) {
+						if ($('#home-calendar').css('display') == 'none') {
+							$('a.calendar-toggle').addClass('active');
+							$('#home-calendar').slideDown();
+						}
+					   $('#wp-calendar').height($('#wp-calendar').width() - $('caption').height() );
+					   return false;
+					});
+			
+					$('#wp-calendar caption').on('click', function (e) {
+						if ($('a.calendar-toggle').hasClass('active') ) {
+							$('a.calendar-toggle').removeClass('active');
+							$('#home-calendar').slideUp();
+						}
+						this.blur();
+						e.preventDefault();
+						return false;
+					});
+				});
+			</script>
